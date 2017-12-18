fe3a98c4d4b86ae053e40c04a884080a06d154bd
marzanita <annmartinez@knights.ucf.edu>
2017-12-17 19:06:11 -0500

    update

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
-// // import the http module
-// var http = require('http');
-// // handle sending request and returning responses
-// function handleRequest(req, res) {
-//     // return string
-//     res.end('Hello world!');
-// }
-// // create the server
-// var server = http.createServer(handleRequest);
-// // start the server and listen on port x
-// server.listen(8080, function() {
-//     console.log('Listening on port 8080');
-// });
-
-
-
-//////////////////////// EXPRESS/ ///////////////////
-
-
-
+// require our dependecies
 var express = require('express');
 var app = express();
 var port = 8080;
 
+// router our app
+var router = require('./app/routes');
+app.use('/', router);
+
 // start the server
 app.listen(port, function() {
     console.log('app started');
-});
-
-// route our app
-
-app.get('/', function(req, res) {
-    res.send('hello world');
 });
